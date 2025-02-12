78c59ffe171b433039e9e66e8f12637e6ba2f479
Melisa Ciftci <ciftcimelisa03@gmail.com>
2025-02-12 20:53:38 +0300

    Add show function to display tasks with indices.

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ def add_task(task):
 
     return f"Task added: {task}"
 
+def show_tasks():
+    try:
+        with open('tasks.txt', 'r') as file:
+            tasks = file.readlines()
+    except FileNotFoundError:
+        tasks = []
+
+    if not tasks:
+        print("There is nothing here.")
+    else:
+        print("Tasks:\n")
+        for index, tasks in enumerate(tasks, start=1):
+            print(f"{index}.{tasks.strip()}")
+
 while True:
     user_input = input("What can I do for you?").strip()
 
@@ -23,3 +37,10 @@ while True:
             print(add_task(task))
         else:
             print("You didn't specify a task.")
+
+    elif 'show tasks' in user_input:
+        print(show_tasks())
+
+    elif 'bye' in user_input:
+        print('Goodbye!')
+        break
